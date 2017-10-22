c1df6653bd127f12dc69a369091429223625c675
Majzer <bence@mozer.hu>
2017-10-22 14:05:32 +0200

    Ballistic osztály néhány alap metódus, képek elhelyezése, háttér beállítása (Csak tesztelésnek)

--- core/src/com/mygdx/game/Ballistics.java
+++ core/src/com/mygdx/game/Ballistics.java
@@ -1,8 +1,42 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+
 /**
  * Created by Majzer on 22/10/2017.
  */
 
 public class Ballistics {
+
+    private float targetY;
+    private float targetX;
+    private float velocity;
+
+    public float getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(float velocity) {
+        this.velocity = velocity;
+    }
+
+    public float getTargetX() {
+        return Gdx.graphics.getWidth()-128;
+    }
+
+    public void setTargetX(float targetX) {
+        this.targetX = targetX;
+    }
+
+    public float getTargetY() {
+        return 0;
+    }
+
+    public void setTargetY(float targetY) {
+        this.targetY = targetY;
+    }
+
+
+
+
 }
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -4,30 +4,45 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture cannon;
+	Texture target;
+	Texture background;
+	Sprite cannonSprite;
+	Sprite targetSprite;
+	Ballistics ballistics = new Ballistics();
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		cannon = new Texture("cannon.png");
+		target = new Texture("target.png");
+		background = new Texture("background.png");
+		cannonSprite = new Sprite(cannon);
+		cannonSprite.setSize(128,128);
+		targetSprite = new Sprite(target);
+		targetSprite.setSize(128,128);
+		targetSprite.setPosition(ballistics.getTargetX(),ballistics.getTargetY());
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(background, 0, 0);
+		cannonSprite.draw(batch);
+		targetSprite.draw(batch);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		cannon.dispose();
 	}
 }
