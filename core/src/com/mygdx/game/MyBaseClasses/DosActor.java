4944a85ba6c94d0085c09cf28396580a0c69bb05
Majzer <bence@mozer.hu>
2017-10-27 12:26:52 +0200

    DOSActor mozgás

--- core/src/com/mygdx/game/MyBaseClasses/DosActor.java
+++ core/src/com/mygdx/game/MyBaseClasses/DosActor.java
@@ -13,14 +13,19 @@ import com.mygdx.game.MyBaseClasses.Scene2D.OneSpriteStaticActor;
 public class DosActor extends OneSpriteStaticActor {
 
 
-    public DosActor(Texture texture) {
+    public DosActor(Texture texture, float x, float y) {
         super(texture);
         setSize(102.4f,76.8f);
+        setPosition(x, y);
     }
 
     @Override
     public void act(float delta) {
         super.act(delta);
+        if(getX() > 0) setX(getX()-1*elapsedTime*10);
+        if(getX()<0) setX(0);
+        if(getY() > 0) setY(getY()-1*elapsedTime*10);
+        if(getY()<0) setY(0);
         if(getWidth()<1024)
         setSize(102.4f*elapsedTime*4,76.8f*elapsedTime*4);
         if(3.0 < elapsedTime)  {
